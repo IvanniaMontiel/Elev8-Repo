0791209e5fa6eb159e7853353b9e53ce489597a2
IvanniaMolina <Ivanniamm23@gmail.com>
2021-12-22 11:11:21 -0600

    Descripcion de los cambios que se hicieron

---
+++ proyectoCalculadora.py
@@ -0,0 +1,7 @@
+print('HOLA CAMARON CON COLA')
+print('ingrese el primer numero')
+numUno=int(input())
+print('ingrese el segundo numero')
+numDos=int(input())
+resultadoSuma = numUno+numDos
+print('la suma de los numeros es', resultadoSuma)
